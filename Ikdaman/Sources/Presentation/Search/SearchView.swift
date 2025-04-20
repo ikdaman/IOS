3687ef4da88327b9f8f312e61106f782042d0c8c
이재혁/파인원 <gur3745@pineone.com>
2025-04-20 16:45:33 +0900

    Search(Fix): 책 제목 검색 클래스 생성

---
+++ Ikdaman/Sources/Presentation/Search/SearchView.swift
@@ -0,0 +1,33 @@
+//
+//  SearchView.swift
+//  Ikdaman
+//
+//  Created by 이재혁 on 4/20/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SearchView: UIView {
+    // MARK: - Properties
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setupLayout() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+}
